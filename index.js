575489f1c991d45e704b1c807d97d85310570dae
bloodchen <31466551+bloodchen@users.noreply.github.com>
2024-11-16 18:07:44 +0800

    update

--- index.js
+++ index.js
@@ -15,27 +15,30 @@ async function startServer() {
     app.addHook("preHandler", async (req, res) => {
         console.log(req.url)
     })
+    regEndpoints()
     const port = process.env.port || 8080
     await app.listen({ port, host: '0.0.0.0' });
     console.log(`Starting ${Config.project.name} service on:`, port)
 }
-dotenv.config()
+dotenv.config({path:"env"})
 async function main() {
     startServer()
     process.on('SIGINT', onExit);
     process.on('SIGTERM', onExit);
 }
-app.get('/', (req, res) => {
-    console.log(req.url)
-    return Config.project.name
-})
-app.get('/test', async (req, res) => {
-
-    return "ok"
-})
-app.post('/logSearch', async (req, res) => {
-    const body = req.body
-    console.log(body)
-    return "ok"
-})
+function regEndpoints(){
+    app.get('/', (req, res) => {
+        console.log(req.url)
+        return Config.project.name
+    })
+    app.get('/test', async (req, res) => {
+    
+        return "ok"
+    })
+    app.post('/logSearch', async (req, res) => {
+        const body = req.body
+        console.log(body)
+        return "ok"
+    })
+}
 main()
