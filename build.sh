5491542fdfbe4adc3ae8a7b8d0b9e29ee3dc84f4
bloodchen <31466551+bloodchen@users.noreply.github.com>
2025-07-05 07:19:14 +0800

    update

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ PORT="${dockerPort:=8100}"
 docker build -t $NAME .
 docker container stop $NAME
 docker container rm $NAME
-docker run --name $NAME --ulimit nofile=90000:90000 -p $PORT:8080 --restart=always -d $NAME
+docker run --name $NAME --ulimit nofile=90000:90000 -p $PORT:8080 --restart=always \
+--log-driver json-file --log-opt max-size=200m --log-opt max-file=3 \
+-d $NAME
--- common/util.js
+++ common/util.js
@@ -4,4 +4,16 @@ export class Util extends BaseService {
     async init(gl) {
 
     }
+    getClientIp(req) {
+        let IP =
+            //req.ip ||
+            req.headers['cf-connecting-ip'] ||
+            req.headers["x-forwarded-for"] ||
+            req.socket.remoteAddress ||
+            req.connection.remoteAddress ||
+            req.connection.socket.remoteAddress;
+        IP = IP.split(',')[0]
+        //IP = IP.split(":").pop()
+        return IP;
+    }
 }
